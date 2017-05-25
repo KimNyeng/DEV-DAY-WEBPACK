7c66d1ab60a3e603d81a653f45614b8a9371eca0
HyunWoo <xodltus@naver.com>
2017-05-25 11:37:39 +0900

    babel scss finished

--- src/js/app.js
+++ src/js/app.js
@@ -1,3 +1,6 @@
+import '../css/main.scss';
+import { RandomGenerator } from './random-generator';
+ 
 const outputParagraph = document.querySelector('#outputParagraph');
 
 const outputRandomInt = () => {
@@ -7,6 +10,9 @@ const outputRandomInt = () => {
 const outputRandomRange = () => {
     outputParagraph.textContent = RandomGenerator.randomRange(1,500);
 };
+
+const buttonRndInt = document.querySelector('#randomInt');
+const buttonRndRange = document.querySelector('#randomRange');
 
 buttonRndInt.addEventListener('click',outputRandomInt)
 buttonRndRange.addEventListener('click', outputRandomRange);
